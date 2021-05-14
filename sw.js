3e5885892b66b7986df28c27912890a9f40251a0
bth <contactbth@gmail.com>
2021-05-14 16:09:12 +0200

    Add offline with service worker (#9)

---
+++ sw.js
@@ -0,0 +1,37 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/colortab/',
+        '/colortab/index.html',
+        '/colortab/img/icon.svg',
+        '/colortab/manifest.webmanifest'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (!(event.request.url.indexOf('http') === 0)) return;
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/colortab/index.html');;
+      });
+    }
+  }));
+});
